81397a98201143435598910a5df4bddd54906a33
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-06-11 10:54:02 +0200

    Added general constraints, reordered code.
     - constraints now must be fully specified. If there is no constraint, add inf.
     - plots now have a title, the X matrix is now transposed compared to last version.

--- inv_pendulum.m
+++ inv_pendulum.m
@@ -6,23 +6,24 @@ A = [ 1.0259     0.5040   0       0;
      -0.0247    -0.0006   0       1];
 B = [-0.0013; -0.0504; 0.0006; 0.025];
 x0 = [0.2; 0; 0; 0]; %Initial state
-%% Restrictions and cost matrices
+%% Cost matrices and constraints
 Q = [ 1      0   0     0; 
       0   0.01   0     0; 
       0      0   1     0; 
       0      0   0  0.01];
 R = 0.01;
-Cx =[ 1 0 0 0; -1 0 0 0];
-Cu = [0; 0];
-dx = [0.2; 0.2];
+dx = [0.2; inf; 1; inf;
+      0.2; inf; 1; inf]; %state constraints, positive and -negative
+du = [inf; inf]; %input constraints
 %% Optimization problem definition
 N = 100;
 nu = size(B,2); %number of inputs
 nx = size(A,1); %number of states
-
-Csmall = [zeros(2,1) Cx]; 
+Cx = [eye(nx); -eye(nx)];
+Cu = [eye(nu); -eye(nu)];
+Csmall = blkdiag(Cu,Cx); 
 Qsmall = blkdiag(R,Q);
-Asmall = [-B eye(size(A,1))];
+Asmall = [-B eye(nx)];
 bsmall= zeros(size(A,1),1);
 C_hat = Csmall;
 Q_hat = Qsmall;
@@ -40,35 +41,38 @@ for i = 1:N-1
     cols_u = i*nu + i*nx;
     A_hat(lines_l: lines_u, cols_l: cols_u)= -A;
 end
-dx_hat = repmat(dx, [N 1]);
+d_hat = repmat([du;dx], [N 1]);
 b_hat = [b_hat; repmat(bsmall, [N-1 1])];
 q = zeros(size(Q_hat,1),1);
 options = optimoptions('quadprog', ...
-    'Algorithm', 'interior-point-convex', 'Display', 'off');
-[Z,FVAL,EXITFLAG] = quadprog(Q_hat, q, C_hat, dx_hat, A_hat, b_hat,[],[],[], options);
-%% Plotting
+    'Algorithm', 'interior-point-convex', 'Display', 'final');
+[Z,FVAL,EXITFLAG] = quadprog(Q_hat, q, C_hat, d_hat, A_hat, b_hat,[],[],[], options);
 X = reshape(Z, 5,[]);
-X = X';
-u = X(:,1);
-X = X(:,2:5);
+u = X(1,:);
+X = X(2:5,:);
+%% Plotting
+t = 0:N-1;
 figure(1);
 subplot(4,2,1);
-plot(u);
+plot(t,u);
 title('Input u');
 grid on
 subplot(4,2,2);
-plot(X(:,1));
+plot(t,X(1,:));
 title('Arm position x_1');
 grid on
 subplot(4,2,4);
-plot(X(:,2));
+plot(t,X(2,:));
 title('Arm speed x_2');
 grid on
 subplot(4,2,6);
-plot(X(:,3));
+plot(t,X(3,:));
 title('Trolley position x_3');
 grid on
 subplot(4,2,8);
-plot(X(:,4));
+plot(t,X(4,:));
 title('Trolley speed x_4');
 grid on
+axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0 1],'Box','off','Visible','off','Units','normalized', 'clipping' , 'off');
+titlestring = sprintf('Run with N=%d, Nc = %d',N,N);
+text(0.5, 1, ['\bf ' titlestring],'HorizontalAlignment','center','VerticalAlignment', 'top')
