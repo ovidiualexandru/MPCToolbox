6fbb0a7ea6d734286eae7d08b812c1935d4c337e
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-06-11 15:20:16 +0200

    Added MPC formulation. Moved QP problem to different function.

--- inv_pendulum.m
+++ inv_pendulum.m
@@ -12,44 +12,25 @@ Q = [ 1      0   0     0;
       0      0   1     0; 
       0      0   0  0.01];
 R = 0.01;
-dx = [0.2; inf; 1; inf;
-      0.2; inf; 1; inf]; %state constraints, positive and -negative
+dx = [0.2; inf; inf; inf;
+      0.2; inf; inf; inf]; %state constraints, positive and -negative
 du = [inf; inf]; %input constraints
-%% Optimization problem definition
-N = 100;
+%% QP solve
+N = 200;
+Nc = 20;
 nu = size(B,2); %number of inputs
 nx = size(A,1); %number of states
-Cx = [eye(nx); -eye(nx)];
-Cu = [eye(nu); -eye(nu)];
-Csmall = blkdiag(Cu,Cx); 
-Qsmall = blkdiag(R,Q);
-Asmall = [-B eye(nx)];
-bsmall= zeros(size(A,1),1);
-C_hat = Csmall;
-Q_hat = Qsmall;
-A_hat = [-B eye(size(A,1))];
-b_hat = A*x0;
-for i = 1:N-1
-    %Add another element to the block diagonal matrices
-    C_hat = blkdiag(C_hat, Csmall);
-    Q_hat = blkdiag(Q_hat, Qsmall);
-    A_hat = blkdiag(A_hat, Asmall);
-    %Add '-A' to the subdiagonal
-    lines_l = i*nx + 1;
-    lines_u = (i+1)*nx;
-    cols_l = i*nu + (i-1)*nx + 1;
-    cols_u = i*nu + i*nx;
-    A_hat(lines_l: lines_u, cols_l: cols_u)= -A;
+X = zeros(nx, N); %save all states, for plotting
+u = zeros(nu, N); %save all inputs
+x = x0;
+for i = 1:N
+    %fprintf('Iteration: %d\n',i);
+    X(:,i) = x; %save current state
+    [ue, Xe] = qp_fullstate(A, B, Q, R, Nc, du, dx, x);    
+    u(i) = ue(1); %use only the first command from predictions
+    x = A*x + B*u(i); %compute next state
+    x = x + 0.01.*rand(nx,1).*x; %add disturbance
 end
-d_hat = repmat([du;dx], [N 1]);
-b_hat = [b_hat; repmat(bsmall, [N-1 1])];
-q = zeros(size(Q_hat,1),1);
-options = optimoptions('quadprog', ...
-    'Algorithm', 'interior-point-convex', 'Display', 'final');
-[Z,FVAL,EXITFLAG] = quadprog(Q_hat, q, C_hat, d_hat, A_hat, b_hat,[],[],[], options);
-X = reshape(Z, 5,[]);
-u = X(1,:);
-X = X(2:5,:);
 %% Plotting
 t = 0:N-1;
 figure(1);
---
+++ qp_fullstate.m
@@ -0,0 +1,37 @@
+function [u, X, FVAL, EXITFLAG] = qp_fullstate(A, B, Q, R, Nc, du, dx, x0)
+%% QP definition
+nu = size(B,2); %number of inputs
+nx = size(A,1); %number of states
+Cx = [eye(nx); -eye(nx)];
+Cu = [eye(nu); -eye(nu)];
+Csmall = blkdiag(Cu,Cx); 
+Qsmall = blkdiag(R,Q);
+Asmall = [-B eye(nx)];
+bsmall= zeros(nx,1);
+C_hat = Csmall;
+Q_hat = Qsmall;
+A_hat = [-B eye(nx)];
+b_hat = A*x0;
+for i = 1:Nc-1
+    %Add another element to the block diagonal matrices
+    C_hat = blkdiag(C_hat, Csmall);
+    Q_hat = blkdiag(Q_hat, Qsmall);
+    A_hat = blkdiag(A_hat, Asmall);
+    %Add '-A' to the subdiagonal
+    lines_l = i*nx + 1;
+    lines_u = (i+1)*nx;
+    cols_l = i*nu + (i-1)*nx + 1;
+    cols_u = i*nu + i*nx;
+    A_hat(lines_l: lines_u, cols_l: cols_u)= -A;
+end
+d_hat = repmat([du;dx], [Nc 1]);
+b_hat = [b_hat; repmat(bsmall, [Nc-1 1])];
+q = zeros(size(Q_hat,1),1);
+%% QP solver
+options = optimoptions('quadprog', ...
+    'Algorithm', 'interior-point-convex', 'Display', 'off');
+[Z,FVAL,EXITFLAG] = quadprog(Q_hat, q, C_hat, d_hat, A_hat, b_hat,[],[],[], options);
+%% Return variables
+X = reshape(Z, nu+nx,[]);
+u = X(1:nu,:);
+X = X(nu+1:end,:);
