c365174267b9557a53dfed84843997da175eb715
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-06-13 10:53:54 +0200

    Added draw_pendulum call in animation_inv_pend to draw and save the pendulum image.

--- animation_inv_pend.m
+++ animation_inv_pend.m
@@ -1,5 +1,5 @@
 %% Tunables
-vid_width = 800;
+vid_width = 1000;
 vid_height = 640;
 %% Preparations
 t = 0:N-1;
@@ -56,12 +56,24 @@ axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0 1],'Box','off','Visible','off',
 titlestring = sprintf('Run with N=%d, Nc = %d',N,Nc);
 text(0.5, 1, ['\bf ' titlestring],'HorizontalAlignment','center','VerticalAlignment', 'top');
 %% Draw dynamic images and save to file
+subplot(4,2, [5,7]);
+im_width = 200;
+im_height = 70;
+y0 = im_height - 15;
+br = 3;
+l = 50;
+t_height = 10;
+t_width = 30;
 for i = 1:N
     set(l1, 'XData', [t(i); t(i)]);
     set(l2, 'XData', [t(i); t(i)]);
     set(l3, 'XData', [t(i); t(i)]);
     set(l4, 'XData', [t(i); t(i)]);
     set(l5, 'XData', [t(i); t(i)]);
+    theta = X(1, i)*pi/2;
+    x0 = -X(3,i)*(im_width/2) + im_width/2;
+    J = draw_pendulum(im_height, im_width, x0, y0, theta, br, l, t_height, t_width);
+    imshow(J);
     drawnow
     frame = getframe(figh); % get the current frame
     im = frame2im(frame); % convert to normal image
