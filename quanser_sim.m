f7eda0670f0e7e0faf7b3a405bc6f16952759b4e
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-06-19 18:01:21 +0200

    Renamed saved data from simulations to more relevant names. Added titles to plots.
     - Xtil (X tilde): nonlinear continous model
     - Xhat : nonlinear pseudo-discrete model (still continous, but with euler discretization)
     - Xbar: SL model.

--- quanser_sim.m
+++ quanser_sim.m
@@ -1,90 +1,95 @@
 clear
 %% Initialization
 x0 = [20; 0; 10; 0; 5; 0]; %Initial state
-N = 150; % samples
-h = 0.05; % s - sampling time
+N = 300; % samples
+h = 0.1; % s - sampling time
 nu = 2;
 nx = length(x0);
 %% Define inputs
 u = zeros(nu, N);
-u(:,11:30) = repmat([1; 3],1,20);
-u(:, 31: 50) = repmat([3; 1],1,20);
-u(:, 51:100) = repmat([1; 1],1,50);
-u(:, 101:150) = repmat([2;2],1,50);
-
+u(:,11:50) = repmat([0.5; 3],1,40);
+u(:, 51: 100) = repmat([3.6; 1],1,50);
+u(:, 101:150) = repmat([1.1; 1.1],1,50);
 %% Continous nonlinear model
-Xc = zeros(nx, N); %save all states, for plotting
+Xtil = zeros(nx, N); %save all states, for plotting
 x = x0;
 for i = 1:N
-    Xc(:,i) = x; %save current state
+    Xtil(:,i) = x; %save current state
     [Tout, Yout] = ode45(@quanser_nonlin_cont, [0 h], [x; u(:,i)]);
-    x = Yout(end, 1:6)'; %get next state
+    x = Yout(end, 1:6)'; %get new state
 end
 %% Discrete nonlinear model - Euler discretization
-Xn = zeros(nx, N); %save all states, for plotting
+Xhat = zeros(nx, N); %save all states, for plotting
 x = x0;
 for i = 1:N
-    Xn(:,i) = x; %save current state
-    [F, G] = quanser_nonlin_disc(x); %get next state
+    Xhat(:,i) = x; %save current state
+    [F, G] = quanser_nonlin_disc(x);
     x = x + h*(F + G*u(:,i));
 end
 %% Discrete linear model - Successive linearizations
-Xl = zeros(nx, N); %save all states, for plotting
+Xbar = zeros(nx, N); %save all states, for plotting
 x = x0;
 [A,B] = quanser_sl_cont(x,u(:,1)); %Initial (A,B) pair
 for i = 1:N
-    Xl(:,i) = x; %save current state
+    Xbar(:,i) = x; %save current state
     [A,B] = quanser_sl_cont(x,u(:,i)); %uncomment this to recalculate
 %     (A,B) for each timestep
     x = x + h*(A*x + B*u(:,i));
 end
+Xbar = zeros(nx, N);
 %% Plotting 
 figure(1);
 t = 0:h:((N-1)*h);
 subplot(6,1,1);
-plot(t, Xc(1,:), 'b-');
+plot(t, Xtil(1,:), 'b-');
+title('Elevation angle $\epsilon$','Interpreter','latex');
 hold on
-plot(t,Xn(1,:), 'r--');
-plot(t,Xl(1,:), 'g:');
+plot(t,Xhat(1,:), 'r--');
+plot(t,Xbar(1,:), 'g:');
 legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
 hold off
 
 subplot(6,1,2);
-plot(t, Xc(2,:), 'b-');
+plot(t, Xtil(2,:), 'b-');
+title('Elevation speed $\dot{\epsilon}$','Interpreter','latex');
 hold on
-plot(t,Xn(2,:), 'r--');
-plot(t,Xl(2,:), 'g:');
+plot(t,Xhat(2,:), 'r--');
+plot(t,Xbar(2,:), 'g:');
 legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
 hold off
 
 subplot(6,1,3);
-plot(t, Xc(3,:), 'b-');
+plot(t, Xtil(3,:), 'b-');
+title('Pitch angle $\theta$','Interpreter','latex');
 hold on
-plot(t,Xn(3,:), 'r--');
-plot(t,Xl(3,:), 'g:');
+plot(t,Xhat(3,:), 'r--');
+plot(t,Xbar(3,:), 'g:');
 legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
 hold off
 
 subplot(6,1,4);
-plot(t, Xc(4,:), 'b-');
+plot(t, Xtil(4,:), 'b-');
+title('Pitch speed $\dot{\theta}$','Interpreter','latex');
 hold on
-plot(t,Xn(4,:), 'r--');
-plot(t,Xl(4,:), 'g:');
+plot(t,Xhat(4,:), 'r--');
+plot(t,Xbar(4,:), 'g:');
 legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
 hold off
 
 subplot(6,1,5);
-plot(t, Xc(5,:), 'b-');
+plot(t, Xtil(5,:), 'b-');
+title('Travel angle $\phi$','Interpreter','latex');
 hold on
-plot(t,Xn(5,:), 'r--');
-plot(t,Xl(5,:), 'g:');
+plot(t,Xhat(5,:), 'r--');
+plot(t,Xbar(5,:), 'g:');
 legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
 hold off
 
 subplot(6,1,6);
-plot(t, Xc(6,:), 'b-');
+plot(t, Xtil(6,:), 'b-');
+title('Travel speed $\dot{\phi}$','Interpreter','latex');
 hold on
-plot(t,Xn(6,:), 'r--');
-plot(t,Xl(6,:), 'g:');
+plot(t,Xhat(6,:), 'r--');
+plot(t,Xbar(6,:), 'g:');
 legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
 hold off
