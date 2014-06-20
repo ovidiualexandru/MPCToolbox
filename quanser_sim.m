49371b30ad95179b70df0c70856ac03df8ae9146
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-06-20 11:50:59 +0200

    Changed plotting method, now showing the states in a 2-by-3 matrix

--- quanser_sim.m
+++ quanser_sim.m
@@ -38,58 +38,63 @@ for i = 1:N
 end
 Xbar = zeros(nx, N);
 %% Plotting 
-figure(1);
 t = 0:h:((N-1)*h);
-subplot(6,1,1);
+
+figure(1);
+set(gcf, 'Units', 'normalized');
+set(gcf, 'Position', [1 0.4 1 0.5]); % set the figure at 50% height screen 2
+
+subplot(2,3,1);
 plot(t, Xtil(1,:), 'b-');
 title('Elevation angle $\epsilon$','Interpreter','latex');
 hold on
 plot(t,Xhat(1,:), 'r--');
 plot(t,Xbar(1,:), 'g:');
-legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
+grid on
+legend('NL ode45', 'NL euler', 'SL', 'Location', 'Best');
 hold off
 
-subplot(6,1,2);
+subplot(2,3,4);
 plot(t, Xtil(2,:), 'b-');
 title('Elevation speed $\dot{\epsilon}$','Interpreter','latex');
 hold on
 plot(t,Xhat(2,:), 'r--');
 plot(t,Xbar(2,:), 'g:');
-legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
+grid on
 hold off
 
-subplot(6,1,3);
+subplot(2,3,2);
 plot(t, Xtil(3,:), 'b-');
 title('Pitch angle $\theta$','Interpreter','latex');
 hold on
 plot(t,Xhat(3,:), 'r--');
 plot(t,Xbar(3,:), 'g:');
-legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
+grid on
 hold off
 
-subplot(6,1,4);
+subplot(2,3,5);
 plot(t, Xtil(4,:), 'b-');
 title('Pitch speed $\dot{\theta}$','Interpreter','latex');
 hold on
 plot(t,Xhat(4,:), 'r--');
 plot(t,Xbar(4,:), 'g:');
-legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
+grid on
 hold off
 
-subplot(6,1,5);
+subplot(2,3,3);
 plot(t, Xtil(5,:), 'b-');
 title('Travel angle $\phi$','Interpreter','latex');
 hold on
 plot(t,Xhat(5,:), 'r--');
 plot(t,Xbar(5,:), 'g:');
-legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
+grid on
 hold off
 
-subplot(6,1,6);
+subplot(2,3,6);
 plot(t, Xtil(6,:), 'b-');
 title('Travel speed $\dot{\phi}$','Interpreter','latex');
 hold on
 plot(t,Xhat(6,:), 'r--');
 plot(t,Xbar(6,:), 'g:');
-legend('NL ode45', 'NL euler', 'SL', 'Location', 'NorthEastOutside');
+grid on
 hold off
