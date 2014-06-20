3f8fae86201ee0402abc22ac0d7848e04653424a
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-06-20 13:24:41 +0200

    Added another discretization method to SL with c2d.
     - Xbarc is for euler discretization, Xbard for c2d.
     - changed plotting format, added black background, x and y labels

--- quanser_sim.m
+++ quanser_sim.m
@@ -1,7 +1,7 @@
 clear
 %% Initialization
 x0 = [20; 0; 10; 0; 5; 0]; %Initial state
-N = 300; % samples
+N = 15; % samples
 h = 0.1; % s - sampling time
 nu = 2;
 nx = 6;
@@ -27,75 +27,112 @@ for i = 1:N
     xd = f(1:6);
     x = x + h*xd;
 end
+%% Succesive Liniarization discrete model
+Xbard = zeros(nx, N); %save all states, for plotting
+x = x0;
+[A,B] = quanser_cont_sl(x,u(:,1)); %Initial (A,B) pair
+C = [1 0 0 0 0 0; 0 0 0 0 1 0];
+for i = 1:N
+    Xbard(:,i) = x; %save current state
+    [A,B] = quanser_cont_sl(x,u(:,i)); %recalculate (A,B) for each timestep    
+    sys = ss(A,B,C,0);
+    sysd = c2d(sys,h);
+    Ad = sysd.a;
+    Bd = sysd.b;
+    x = Ad*x + Bd*u(:,i);
+end
+% Xbard = zeros(nx, N);
 %% Succesive Liniarization model with euler discretization
-Xbar = zeros(nx, N); %save all states, for plotting
+Xbarc = zeros(nx, N); %save all states, for plotting
 x = x0;
 [A,B] = quanser_cont_sl(x,u(:,1)); %Initial (A,B) pair
 for i = 1:N
-    Xbar(:,i) = x; %save current state
+    Xbarc(:,i) = x; %save current state
     [A,B] = quanser_cont_sl(x,u(:,i)); %recalculate (A,B) for each timestep
     xd = A*x + B*u(:,i);
     x = x + h*xd;
 end
-% Xbar = zeros(nx, N);
+% Xbarc = zeros(nx, N);
 %% Plotting 
-t = 0:h:((N-1)*h);
+% t = 0:h:((N-1)*h);
+t = 1:N;
 
 figure(1);
+clf
 set(gcf, 'Units', 'normalized');
 set(gcf, 'Position', [1 0.4 1 0.5]); % set the figure at 50% height screen 2
+whitebg([0 0 0]);
+
 
 subplot(2,3,1);
-plot(t, Xtil(1,:), 'b-');
+plot(t,Xtil(1,:), 'b-');
 title('Elevation angle $\epsilon$','Interpreter','latex');
 hold on
-plot(t,Xhat(1,:), 'r--');
-plot(t,Xbar(1,:), 'g:');
+plot(t,Xhat(1,:), 'r:');
+plot(t,Xbard(1,:), 'c--');
+plot(t,Xbarc(1,:), 'g:');
+xlabel('[k]');
+ylabel('[deg]');
 grid on
-legend('NL ode45', 'NL euler', 'SL', 'Location', 'Best');
+legend('NL ode45', 'NL euler', 'SL c2d', 'SL euler', 'Location', 'Best');
 hold off
 
 subplot(2,3,4);
-plot(t, Xtil(2,:), 'b-');
+plot(t,Xtil(2,:), 'b-');
 title('Elevation speed $\dot{\epsilon}$','Interpreter','latex');
 hold on
-plot(t,Xhat(2,:), 'r--');
-plot(t,Xbar(2,:), 'g:');
+plot(t,Xhat(2,:), 'r:');
+plot(t,Xbard(2,:), 'c--');
+plot(t,Xbarc(2,:), 'g:');
+xlabel('[k]');
+ylabel('[deg/s]');
 grid on
 hold off
 
 subplot(2,3,2);
-plot(t, Xtil(3,:), 'b-');
+plot(t,Xtil(3,:), 'b-');
 title('Pitch angle $\theta$','Interpreter','latex');
 hold on
-plot(t,Xhat(3,:), 'r--');
-plot(t,Xbar(3,:), 'g:');
+plot(t,Xhat(3,:), 'r:');
+plot(t,Xbard(3,:), 'c--');
+plot(t,Xbarc(3,:), 'g:');
+xlabel('[k]');
+ylabel('[deg]');
 grid on
 hold off
 
 subplot(2,3,5);
-plot(t, Xtil(4,:), 'b-');
+plot(t,Xtil(4,:), 'b-');
 title('Pitch speed $\dot{\theta}$','Interpreter','latex');
 hold on
-plot(t,Xhat(4,:), 'r--');
-plot(t,Xbar(4,:), 'g:');
+plot(t,Xhat(4,:), 'r:');
+plot(t,Xbard(4,:), 'c--');
+plot(t,Xbarc(4,:), 'g:');
+xlabel('[k]');
+ylabel('[deg/s]');
 grid on
 hold off
 
 subplot(2,3,3);
-plot(t, Xtil(5,:), 'b-');
+plot(t,Xtil(5,:), 'b-');
 title('Travel angle $\phi$','Interpreter','latex');
 hold on
-plot(t,Xhat(5,:), 'r--');
-plot(t,Xbar(5,:), 'g:');
+plot(t,Xhat(5,:), 'r:');
+plot(t,Xbard(5,:), 'c--');
+plot(t,Xbarc(5,:), 'g:');
+xlabel('[k]');
+ylabel('[deg]');
 grid on
 hold off
 
 subplot(2,3,6);
-plot(t, Xtil(6,:), 'b-');
+plot(t,Xtil(6,:), 'b-');
 title('Travel speed $\dot{\phi}$','Interpreter','latex');
 hold on
-plot(t,Xhat(6,:), 'r--');
-plot(t,Xbar(6,:), 'g:');
+plot(t,Xhat(6,:), 'r:');
+plot(t,Xbard(6,:), 'c--');
+plot(t,Xbarc(6,:), 'g:');
+xlabel('[k]');
+ylabel('[deg/s]');
 grid on
 hold off
