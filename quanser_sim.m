bfe757e46db2348c944639090ffb99b0ef27cd7c
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-06-26 16:25:01 +0200

    Renamed input matrix from u to U

--- quanser_sim.m
+++ quanser_sim.m
@@ -16,19 +16,19 @@ nu = 2;
 nx = 6;
 l = 5; %SL horizon i.e. how many steps until a new affine term is calculated
 %% Input signal shape
-u = ones(nu, N);
+U = ones(nu, N);
 % u(:, 1:30) = repmat([3; 3],1,30);
 % u(:, 31: 60) = repmat([5; 0],1,30);
 % u(:, 61:90) = repmat([0; 5],1,30);
-u(1,1:5) = [2 3 2.3 0 3];
-u(2,1:5) = [2.2 2.1 1.3 4 3];
-u(:,6:10) = ones(2,5)*3;
+U(1,1:5) = [2 3 2.3 0 3];
+U(2,1:5) = [2.2 2.1 1.3 4 3];
+U(:,6:10) = ones(2,5)*3;
 %% Nonlinear model with ode45
 Xtil = zeros(nx, N); %save all states, for plotting
 x = x0;
 for i = 1:N
     Xtil(:,i) = x; %save current state
-    [Tout, Yout] = ode45(@quanser_cont_nl, [0 h], [x; u(:,i)]);
+    [Tout, Yout] = ode45(@quanser_cont_nl, [0 h], [x; U(:,i)]);
     x = Yout(end, 1:6)'; %get new state
 end
 %% Nonlinear model with euler discretization
@@ -36,19 +36,19 @@ Xhat = zeros(nx, N); %save all states, for plotting
 x = x0;
 for i = 1:N
     Xhat(:,i) = x; %save current state
-    f = quanser_cont_nl([],[x; u(:,i)]);
+    f = quanser_cont_nl([],[x; U(:,i)]);
     xd = f(1:6);
     x = x + h*xd;
 end
 %% Succesive Liniarization model, discretized with c2d 
 Xbard = zeros(nx, N); %save all states, for plotting
 x = x0;
-[A,B,g] = quanser_cont_sl(x,u(:,1)); %Initial (A,B) pair
+[A,B,g] = quanser_cont_sl(x,U(:,1)); %Initial (A,B) pair
 C = [1 0 0 0 0 0; 0 0 0 0 1 0];
 for i = 1:N
     Xbard(:,i) = x; %save current state
     if mod(i,l) == 0
-        [A,B,g] = quanser_cont_sl(x,u(:,i)); %recalculate (A,B,g)
+        [A,B,g] = quanser_cont_sl(x,U(:,i)); %recalculate (A,B,g)
     end
     sys = ss(A,B,C,0);
     sysd = c2d(sys,h, 'zoh');
@@ -56,19 +56,19 @@ for i = 1:N
     sysd.b = h*B;
     Ad = sysd.a;
     Bd = sysd.b;
-    x = Ad*x + Bd*u(:,i) + h*g;
+    x = Ad*x + Bd*U(:,i) + h*g;
 end
 % Xbard = zeros(nx, N);
 %% Succesive Liniarization model with euler discretization
 Xbarc = zeros(nx, N); %save all states, for plotting
 x = x0;
-[A,B,g] = quanser_cont_sl(x,u(:,1)); %Initial (A,B) pair
+[A,B,g] = quanser_cont_sl(x,U(:,1)); %Initial (A,B) pair
 for i = 1:N
     Xbarc(:,i) = x; %save current state
     if mod(i,l) == 0
-        [A,B,g] = quanser_cont_sl(x,u(:,i)); %recalculate (A,B,g)
+        [A,B,g] = quanser_cont_sl(x,U(:,i)); %recalculate (A,B,g)
     end
-    xd = A*x + B*u(:,i) + g;
+    xd = A*x + B*U(:,i) + g;
     x = x + h*xd;
 end
 % Xbarc = zeros(nx, N);
@@ -83,7 +83,7 @@ whitebg([0 0 0]);
 %Plot the input 3 times, for each state pair
 for i = 1:3
     subplot(3,3,i);
-    plot(t, u(1,:) ,'y--', t, u(2,:), 'c--');
+    plot(t, U(1,:) ,'y--', t, U(2,:), 'c--');
     title('Inputs');
     grid on
     xlabel('[k]');
