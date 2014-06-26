a25df504e92ad18176dd54dc0d2e3ac5f7c4f5d1
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-06-26 12:44:54 +0200

    Fixed inside-loop state and input calculations, off-by-1 bugs.
     - better code layout for simulation
     - minor changes for the rest, removed unused code.

--- quanser_lqr.m
+++ quanser_lqr.m
@@ -1,49 +1,34 @@
 clear
-%% System
-x0 = [10; 0; 20; 0; 10; 0]; %Initial state
-Vf = 1.8865;
-Vb = 1.936;
+%% System initialization
+x0 = [45; 2; 25; 0; 30; 0]; %Initial state
+u0 = [1.8865; 1.936]; % [Vf Vb] initial inputs
 N = 2000; % samples
 h = 0.1; % s - sampling time
 nu = 2;
 nx = 6;
-Nc = 6; % control and prediction horizon
-%% Cost matrices and constraints
-Q = diag([3, 10, 1, 5, 10, 2],0);
-% Q = eye(6)*1;
-R = diag([0.1, 0.1],0);
-% R = eye(2)*1;
-dx = [inf; inf; inf; inf; inf; inf;
-      -inf; -inf; -inf; -inf; -inf; -inf]; %state constraints, positive and negative
-du = [inf; inf;
-     -inf; -inf]; %input constraints
-%% LQR solve
+Nc = 10; % control and prediction horizon
+%% Cost matrices
+Q = diag([10, 0.001, 10, 0.001, 10, 0.001],0);
+R = diag([0.01, 0.01],0);
+%% Solver initialization
 X = zeros(nx, N); %save all states, for plotting
-u = zeros(nu, N); %save all inputs
-u(:,1) = [Vf; Vb];
+U = zeros(nu, N); %save all inputs
+u = u0;
 x = x0;
-C = [1 0 0 0 0 0; 0 0 0 0 1 0];
-% [A,B,g] = quanser_cont_sl(x,u(:,1)); %Initial (A,B) pair
-[A,B,g] = quanser_cont_sl(x,u(:,1)); %recalculate (A,B,g)
+X(:,1) = x;
+U(:,1) = u;
+[A,B,g] = quanser_cont_sl(x,u); %Initial (A,B,g) pair
 K = lqr(A,B,Q,R,0);
-% u_o = u(:,1);
-% x_o = A\(-g-B*u(:,1));
-% x_o = linsolve(A, -g - B*u(:,1));
 x_o = x;
-% u_o = B\(-g - A*x);
 u_o = linsolve(B, -g - A*x);
-for i = 1:N
-    X(:,i) = x; %save current state
+%% LQR solve
+for i = 2:N
+    [Tout, Yout] = ode45(@quanser_cont_nl, [0 h], [x; u]); %f(xk, uk)
+    x = Yout(end, 1:6)'; %get new state, i.e. x = x(k)
     if mod(i,Nc) == 0
-        [A,B,g] = quanser_cont_sl(x,u(:,i)); %recalculate (A,B,g)
+        [A,B,g] = quanser_cont_sl(x,u); %recalculate (A,B,g)
         K = lqr(A,B,Q,R,0);
-        %% Solution one: fix u_o
-%         u_o = u(:,i);
-%         x_o = A\(-g-B*u(:,i));
-%         x_o = linsolve(a, -g - B*u(:,i);
-        %% Solution two: fix x_o
         x_o = x;
-         % u_o = B\(-g - A*x);
         u_o = linsolve(B, -g - A*x);
         fprintf('%d ', i);
         if mod(i,20*Nc) == 0
@@ -51,10 +36,9 @@ for i = 1:N
         end
     end
     ubar = -K*(x - x_o);
-    u(:,i) = ubar + u_o;
-    [Tout, Yout] = ode45(@quanser_cont_nl, [0 h], [x; u(:,i)]);
-    x = Yout(end, 1:6)'; %get new state
-%     x = x + 0.0.*rand(nx,1).*x ; %add noise and disturbance
+    u = ubar + u_o; % new input
+    X(:,i) = x; % save states
+    U(:,i) = u; % save inputs
 end
 %% Plotting
 tk = 1:N;
