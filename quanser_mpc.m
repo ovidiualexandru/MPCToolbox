be8e64f84d50ddfe2f8850f2766d791f904ff832
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-06-26 16:27:53 +0200

    Updated quanser_mpc simulation.
     - changed tunable variables: initial states, control and pred. horizon, tuning matrices and simulation horizon
     - changed cells for better separation: now the initialization phase has separate cell
     - updated the calculation for closer to reality conditions - as in quanser_lqr

--- quanser_mpc.m
+++ quanser_mpc.m
@@ -1,41 +1,61 @@
 clear
-%% System
-x0 = [45; 0; 5; 0; 30; 0]; %Initial state
-N = 30; % samples
+%% System initialization
+x0 = [20; 0; 10; 0; 0; 0]; %Initial state
+u0 = [2; 2]; % [Vf Vb] initial inputs
+N = 2000; % samples
 h = 0.1; % s - sampling time
 nu = 2;
 nx = 6;
-Nc = 5; % control and prediction horizon
+Nc = 4; % control and prediction horizon
 %% Cost matrices and constraints
 Q = diag([1, 0.1, 1, 0.1, 1, 0.1],0);
-R = diag([0.2,0.2],0);
+R = diag([3, 3],0);
 dx = [inf; inf; inf; inf; inf; inf;
       -inf; -inf; -inf; -inf; -inf; -inf]; %state constraints, positive and negative
 du = [inf; inf;
-     -inf; -inf]; %input constraints
-%% QP solve
+      -inf; -inf]; %input constraints
+%% Solver initialization
 X = zeros(nx, N); %save all states, for plotting
-u = zeros(nu, N); %save all inputs
+U = zeros(nu, N); %save all inputs
 x = x0;
+u = u0;
+[A,B,g] = quanser_cont_sl(x,u); %Initial (A,B,g) pair
 C = [1 0 0 0 0 0; 0 0 0 0 1 0];
-% [A,B,g] = quanser_cont_sl(x,u(:,1)); %Initial (A,B) pair
+sys = ss(A,B,C,0);
+sysd = c2d(sys,h, 'zoh');
+sysd.a = eye(nx) + h*A;
+sysd.b = h*B;
+Ad = sysd.a;
+Bd = sysd.b;
+x_o = x;
+u_o = linsolve(B, -g - A*x);
+%% MPC solve
 for i = 1:N
-    X(:,i) = x; %save current state
-    if mod(i,l) == 0
-        [A,B,g] = quanser_cont_sl(x,u(:,i)); %recalculate (A,B,g)
-    end
-    sys = ss(A,B,C,0);
-    sysd = c2d(sys,h, 'zoh');
-    Ad = sysd.a;
-    Bd = sysd.b;
+    X(:,i) = x; % save states
+    U(:,i) = u; % save inputs
+    [Tout, Yout] = ode45(@quanser_cont_nl, [0 h], [x; u]); %f(xk, uk)
+    x = Yout(end, 1:6)'; %get new state, i.e. x = x(k)
     [ue, Xe] = qp_fullstate(Ad, Bd, Q, R, Nc, du, dx, x);
-    u(:,i) = ue(:,1); %use only the first command from predictions
-    [Tout, Yout] = ode45(@quanser_cont_nl, [0 h], [x; u(:,i)]);
-    x = Yout(end, 1:6)'; %get new state
-    x = x + 0.0.*rand(nx,1).*x ; %add noise and disturbance
+    ubar = ue(:,1); %use only the first command from predictions
+    u = ubar + u_o;
+    if mod(i,Nc) == 0
+        [A,B,g] = quanser_cont_sl(x,u); %recalculate (A,B,g)
+        sys = ss(A,B,C,0);
+        sysd = c2d(sys,h, 'zoh');
+%         sysd.a = eye(nx) + h*A;
+%         sysd.b = h*B;
+        Ad = sysd.a;
+        Bd = sysd.b;
+        x_o = x;
+        u_o = linsolve(B, -g - A*x);
+        fprintf('%d ', i);
+        if mod(i,20*Nc) == 0
+            fprintf('\n');
+        end
+    end
 end
 %% Plotting
-t = 1:N;
+tk = 1:N;
 
 figure(1);
 clf;
@@ -44,19 +64,20 @@ whitebg([0 0 0]);
 %Plot the input 3 times, for each state pair
 for i = 1:3
     subplot(3,3,i);
-    plot(t, u(1,:) ,'y--', t, u(2,:), 'c--');
-    title('Inputs');
-    grid on
-    xlabel('[k]');
+    plot(tk, U(1,:) ,'y--', tk, U(2,:), 'c:');
+    xlabel('samples [k]');
     ylabel('[volts]');
+    grid on
+    hold off
     if i == 1
         legend('Vf', 'Vb', 'Location', 'Best');
     end
+    title('Inputs');
 end
 
 %Plot the states
 subplot(3,3,1+3);
-plot(t,X(1,:), 'b-');
+plot(tk,X(1,:), 'b-');
 title('Elevation angle $\epsilon$','Interpreter','latex');
 xlabel('[k]');
 ylabel('[deg]');
@@ -64,35 +85,35 @@ grid on
 legend('NL ode45', 'NL euler', 'SL c2d', 'SL euler', 'Location', 'Best');
 
 subplot(3,3,4+3);
-plot(t,X(2,:), 'b-');
+plot(tk,X(2,:), 'b-');
 title('Elevation speed $\dot{\epsilon}$','Interpreter','latex');
 xlabel('[k]');
 ylabel('[deg/s]');
 grid on
 
 subplot(3,3,2+3);
-plot(t,X(3,:), 'b-');
+plot(tk,X(3,:), 'b-');
 title('Pitch angle $\theta$','Interpreter','latex');
 xlabel('[k]');
 ylabel('[deg]');
 grid on
 
 subplot(3,3,5+3);
-plot(t,X(4,:), 'b-');
+plot(tk,X(4,:), 'b-');
 title('Pitch speed $\dot{\theta}$','Interpreter','latex');
 xlabel('[k]');
 ylabel('[deg/s]');
 grid on
 
 subplot(3,3,3+3);
-plot(t,X(5,:), 'b-');
+plot(tk,X(5,:), 'b-');
 title('Travel angle $\phi$','Interpreter','latex');
 xlabel('[k]');
 ylabel('[deg]');
 grid on
 
 subplot(3,3,6+3);
-plot(t,X(6,:), 'b-');
+plot(tk,X(6,:), 'b-');
 title('Travel speed $\dot{\phi}$','Interpreter','latex');
 xlabel('[k]');
 ylabel('[deg/s]');
