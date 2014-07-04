7c5797881c60dc252f011c5875a55ef47131614b
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-07-04 10:05:59 +0200

    Added code to mpc_nonlin function.
    - added problem definition as for quadprog program.
     - nonlinear constraint function must be finished.

--- mpc_nonlin.m
+++ mpc_nonlin.m
@@ -1,25 +1,48 @@
-function [u,Z, FVAL, EXITFLAG] = mpc_nonlin(handle_nonlindisc, x0, Q, R, Nc)
-%Nonlinear constraints for fmincon
+function [u, X, FVAL, EXITFLAG] = mpc_nonlin(hande_nlmodeld, Q, R, Nc, du, dx, x0)
+%% Nonlinear constraints function
     function [C,Ceq] = nonlconfunc(z)
         %De for nu pot scapa
         % C e mereu 0
         % Ceq 'simuleaza' toate starile viitoare din X
         % ToDo:
         % - descompune X in [u x]
-        C = 0;
-        for i = 1:L
-            x = nonlindisc(x,u,h);
+        C = zeros(size(z));
+        Xl = reshape(z, nu+nx,[]);
+        ul = Xl(1:nu,:);
+        Xl = Xl(nu+1:end,:);
+        for i = 1:Nc
+            x = hande_nlmodeld(x,ul,h);
             Ceq(:) = z - x;
         end
     end
-
-if ~isa(handle_nonlindisc, 'function_handle')
-    error('No function handle passed');
+%% Argument processing
+if ~isa(hande_nlmodeld, 'function_handle')
+    error('handle_nlmodeld must be a function handle.');
 end
-%% Solve nonlinear problem
-Z = fmincon(@(z) z'*Q_hat*z, x0, A,B,[],[],[],[], @nonlconfunc);
+%% QP definition
+nu = size(du,2); %number of inputs
+nx = size(dx,2); %number of states
+du(2,:) = -du(2,:); %convert negative constraints to positive
+dx(2,:) = -dx(2,:); %convert negative constraints to positive
+du = reshape(du',[],1); %reshape du into a vector
+dx = reshape(dx',[],1); %reshape dx into a vector
+Cx = [eye(nx); -eye(nx)];
+Cu = [eye(nu); -eye(nu)];
+Csmall = blkdiag(Cu,Cx); 
+Qsmall = blkdiag(R,Q);
+C_hat = Csmall;
+Q_hat = Qsmall;
+for i = 1:Nc-1
+    %Add another element to the block diagonal matrices
+    C_hat = blkdiag(C_hat, Csmall);
+    Q_hat = blkdiag(Q_hat, Qsmall);
 end
-
-
-
-
+d_hat = repmat([du;dx], [Nc 1]);
+q = zeros(size(Q_hat,1),1);
+%% Nonlinear solver
+[Z ,FVAL,EXITFLAG] = fmincon(@(z) z'*Q_hat*z + q'*z, x0, C_hat, d_hat,[],[],[],[], @nonlconfunc);
+%% Return variables
+X = reshape(Z, nu+nx,[]);
+u = X(1:nu,:);
+X = X(nu+1:end,:);
+end
