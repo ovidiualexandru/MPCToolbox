e72a7925a98ce940d8810ac846426c2f1d1ed56e
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-07-07 10:46:30 +0200

    Updated animation script to latest version.

--- pendulum/animation_inv_pend.m
+++ pendulum/animation_inv_pend.m
@@ -1,3 +1,5 @@
+addpath('./pendulum');
+addpath('./util');
 %% Tunables
 vid_width = 1000;
 vid_height = 700;
@@ -9,47 +11,47 @@ set(gcf, 'Position', [100 100 vid_width vid_height]);
 %% Draw static images
 h1 = subplot(4,2,1);
 plot(t,u);
-rescaleYLim(gca, [constraints_u(2) constraints_u(1)]*1.1);
+rescaleYLim(gca, [du(2) du(1)]*1.1);
 grid on
 title('Input u');
-line([0;N],[constraints_u(1);constraints_u(1)], 'LineStyle', '--', 'Color', [1 0 0]); %%Upper bound
-line([0;N],[constraints_u(2);constraints_u(2)], 'LineStyle', '--', 'Color', [1 0 0]); %%Lower bound
+line([0;N],[du(1);du(1)], 'LineStyle', '--', 'Color', [1 0 0]); %%Upper bound
+line([0;N],[du(2);du(2)], 'LineStyle', '--', 'Color', [1 0 0]); %%Lower bound
 l1 = line([t(1);t(1)],get(gca,'YLim'), 'LineStyle', '--', 'Color', [0 1 0]);
 
 subplot(4,2,2);
 plot(t,X(1,:));
-rescaleYLim(gca, [constraints_x(2,1) constraints_x(1,1)]*1.1);
+rescaleYLim(gca, [dx(2,1) dx(1,1)]*1.1);
 grid on
 title('Arm position x_1');
-line([0;N],[constraints_x(1,1);constraints_x(1,1)], 'LineStyle', '--', 'Color', [1 0 0]); %%Upper bound
-line([0;N],[constraints_x(2,1);constraints_x(2,1)], 'LineStyle', '--', 'Color', [1 0 0]); %%Lower bound
+line([0;N],[dx(1,1);dx(1,1)], 'LineStyle', '--', 'Color', [1 0 0]); %%Upper bound
+line([0;N],[dx(2,1);dx(2,1)], 'LineStyle', '--', 'Color', [1 0 0]); %%Lower bound
 l2 = line([t(1);t(1)],get(gca,'YLim'), 'LineStyle', '--', 'Color', [0 1 0]);
 
 subplot(4,2,4);
 plot(t,X(2,:));
-rescaleYLim(gca, [constraints_x(2,2) constraints_x(1,2)]*1.1);
+rescaleYLim(gca, [dx(2,2) dx(1,2)]*1.1);
 grid on
 title('Arm speed x_2');
-line([0;N],[constraints_x(1,2);constraints_x(1,2)], 'LineStyle', '--', 'Color', [1 0 0]); %%Upper bound
-line([0;N],[constraints_x(2,2);constraints_x(2,2)], 'LineStyle', '--', 'Color', [1 0 0]); %%Lower bound
+line([0;N],[dx(1,2);dx(1,2)], 'LineStyle', '--', 'Color', [1 0 0]); %%Upper bound
+line([0;N],[dx(2,2);dx(2,2)], 'LineStyle', '--', 'Color', [1 0 0]); %%Lower bound
 l3 = line([t(1);t(1)],get(gca,'YLim'), 'LineStyle', '--', 'Color', [0 1 0]);
 
 subplot(4,2,6);
 plot(t,X(3,:));
 grid on
-rescaleYLim(gca, [constraints_x(2,3) constraints_x(1,3)]*1.1); 
+rescaleYLim(gca, [dx(2,3) dx(1,3)]*1.1); 
 title('Trolley position x_3');
-line([0;N],[constraints_x(1,3);constraints_x(1,3)], 'LineStyle', '--', 'Color', [1 0 0]); %%Upper bound
-line([0;N],[constraints_x(2,3);constraints_x(2,3)], 'LineStyle', '--', 'Color', [1 0 0]); %%Lower bound
+line([0;N],[dx(1,3);dx(1,3)], 'LineStyle', '--', 'Color', [1 0 0]); %%Upper bound
+line([0;N],[dx(2,3);dx(2,3)], 'LineStyle', '--', 'Color', [1 0 0]); %%Lower bound
 l4 = line([t(1);t(1)],get(gca,'YLim'), 'LineStyle', '--', 'Color', [0 1 0]);
 
 subplot(4,2,8);
 plot(t,X(4,:));
-rescaleYLim(gca, [constraints_x(2,4) constraints_x(1,4)]*1.1);
+rescaleYLim(gca, [dx(2,4) dx(1,4)]*1.1);
 grid on
 title('Trolley speed x_4');
-line([0;N],[constraints_x(1,4);constraints_x(1,4)], 'LineStyle', '--', 'Color', [1 0 0]); %%Upper bound
-line([0;N],[constraints_x(2,4);constraints_x(2,4)], 'LineStyle', '--', 'Color', [1 0 0]); %%Lower bound
+line([0;N],[dx(1,4);dx(1,4)], 'LineStyle', '--', 'Color', [1 0 0]); %%Upper bound
+line([0;N],[dx(2,4);dx(2,4)], 'LineStyle', '--', 'Color', [1 0 0]); %%Lower bound
 l5 = line([t(1);t(1)],get(gca,'YLim'), 'LineStyle', '--', 'Color', [0 1 0]);
 
 axes('Position',[0 0 1 1],'Xlim',[0 1],'Ylim',[0 1],'Box','off','Visible','off','Units','normalized', 'clipping' , 'off');
@@ -86,3 +88,5 @@ for i = 1:N
         imwrite(A,map,'animation.gif','gif','WriteMode','append','DelayTime',1/24);
     end
 end
+rmpath('./pendulum');
+rmpath('./util');
