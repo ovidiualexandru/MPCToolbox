2e2985abb88fdc6c036514dc3b542c1248b64dc0
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-07-07 14:44:34 +0200

    Added changes into mpt formulation for quanser.

--- quanser_mpc_mpt.m
+++ quanser_mpc_mpt.m
@@ -2,19 +2,19 @@ clear
 addpath('./quanser');
 addpath('./util');
 %% System initialization
-x0 = [15; 0; 0; 0; 20; 0]; %Initial state
+x0 = [-5; 0; -3; 0; -2; 0]; %Initial state
 u0 = [2; 2]; % [Vf Vb] initial inputs
 N = 1000; % samples
 h = 0.1; % s - sampling time
 nu = 2;
 nx = 6;
-Np = 5; % control and prediction horizon
-Nc = 5;
+Np = 3; % control and prediction horizon
+Nc = 3;
 %% Cost matrices and constraints
-Q = diag([1, 1, 1, 1, 1, 1],0);
-R = diag([1, 1],0);
-dx = [45, inf, 50, inf, inf, inf;
-      -45, -inf, -50, -inf, -inf, -inf]; %state constraints, positive and negative
+Q = diag([10, 1, 10, 1, 10, 1],0);
+R = diag([0.01, 0.01],0);
+dx = [60, inf, 45, inf, 180, inf;
+      -60, -inf, -45, -inf, -180, -inf]; %state constraints, positive and negative
 du = [5, 5;
       0, 0]; %input constraints
 %% Solver initialization
@@ -22,44 +22,32 @@ X = zeros(nx, N); %save all states, for plotting
 U = zeros(nu, N); %save all inputs
 x = x0;
 u = u0;
-C = [1 0 0 0 0 0; 0 0 1 0 0 0];
-[A,B,g] = quanser_cont_sl(x,u); %Initial (A,B,g) pair
-Ad = eye(nx) + h*A;
-Bd = h*B;
-x_o = x;
-u_o = linsolve(B, -g - A*x);
-sys = LTISystem('A', Ad, 'B', Bd, 'Ts', h); % check this?
-ctrl = MPCController(sys, Nc);
-ctrl.model.x.min = dx(2,:)';
-ctrl.model.x.max = dx(1,:)';
-ctrl.model.u.min = du(2,:)'-u_o;
-ctrl.model.u.max = du(1,:)'-u_o;
-ctrl.model.x.penalty = QuadFunction(Q);
-ctrl.model.u.penalty = QuadFunction(R);
 %% MPC solve
 for i = 1:N
     %% Update SL Model
-    if mod(i,Np) == 0
+    if mod(i,Np) == 0 || i == 1
         [A,B,g] = quanser_cont_sl(x,u); %recalculate (A,B,g)
+        [x_o, u_o] = affine_eq(A,B,g);
+        du_bar = du - repmat(u_o',2,1);
+        dx_bar = dx - repmat(x_o',2,1);
         Ad = eye(nx) + h*A;
         Bd = h*B;
-        x_o = x;
-        u_o = linsolve(B, -g - A*x);
         sys = LTISystem('A', Ad, 'B', Bd, 'Ts', h); % check this?
         ctrl = MPCController(sys, Nc);
-        ctrl.model.x.min = dx(2,:)';
-        ctrl.model.x.max = dx(1,:)';
-        ctrl.model.u.min = du(2,:)'-u_o;
-        ctrl.model.u.max = du(1,:)'-u_o;
+        ctrl.model.x.min = dx_bar(2,:)';
+        ctrl.model.x.max = dx_bar(1,:)';
+        ctrl.model.u.min = du_bar(2,:)';
+        ctrl.model.u.max = du_bar(1,:)';
         ctrl.model.x.penalty = QuadFunction(Q);
-        ctrl.model.x.penalty = QuadFunction(Q);
+        ctrl.model.u.penalty = QuadFunction(R);
         fprintf('%d ', i);
         if mod(i,20*Np) == 0
             fprintf('\n');
         end
     end
     %% Get next command
-    ubar = ctrl.evaluate(x-x_o);
+    xbar = x - x_o;
+    ubar = ctrl.evaluate(xbar);
     u = ubar + u_o;
     %% Data logging
     X(:,i) = x; % save states
