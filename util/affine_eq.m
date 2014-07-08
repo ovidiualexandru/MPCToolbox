39e3f785b9b1ab28c1e8145bd3c9b3ba8de3fc7c
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-07-08 15:47:15 +0200

    Rewritten affine_eq function to calculate x_o as being 0.

--- util/affine_eq.m
+++ util/affine_eq.m
@@ -2,8 +2,7 @@ function [x_o, u_o] = affine_eq(A,B,g)
 %AFFINE_EQ Summary of this function goes here
 %   Detailed explanation goes here
 nx = size(A,1);
-n = linsolve([A B], -g);
-x_o = n(1:nx);
-u_o = n(nx + 1:end);
+u_o = linsolve([B], -g);
+x_o = zeros(nx,1);
 end
 
