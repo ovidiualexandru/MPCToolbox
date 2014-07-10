b152a33f9015456bceab19683c63a7ae442dc71b
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-07-10 16:17:27 +0200

    Implemented code for lmpc_condensed function.

--- util/lmpc_condensed.m
+++ util/lmpc_condensed.m
@@ -14,38 +14,66 @@ du = reshape(du',[],1); %reshape du into a vector
 dx = reshape(dx',[],1); %reshape dx into a vector
 Cx = [eye(nx); -eye(nx)];
 Cu = [eye(nu); -eye(nu)];
-Csmall = blkdiag(Cu,Cx);
+%% ABbar
+ABbarsmall = B;
+ABbar = zeros(Nc*nx, Nc*nu);
 for i = 1:Nc
     %Add another element to the block diagonal matrices
     %Add elements to ABbar
-    ABbar = 0;
-    Ap = 0; % Ap = [Ap A^i]
-    Cxbar = 0; %Cxbar = blkdiag(Cxbar, Cx)
-    Cz2 = 0; %Cz2 = blkdiag(Cz2, Cu)
+    for j = 1:(Nc + 1 - i)
+        lstart = (i-1)*nx + (j-1)*nx + 1;
+        lend = lstart + nx - 1;
+        cstart = (j-1)*nu + 1;
+        cend = cstart + nu - 1;
+        ABbar(lstart:lend, cstart:cend) = ABbarsmall;
+    end
+    ABbarsmall = A*ABbarsmall;
 end
-Cz1 = 0; %Cxbar*ABbar
-dz1 = 0; %dxbar - Cxbar*Ap*x0
-dz2 = 0; %repmat(du,[Nc 1]);
-C_hat = 0; % [Cz1; Cz2]
-d_hat = 0; % [dz1; dz2]
+%% Ap
+Ap = [];
+Apsmall = A;
+for i = 1:Nc
+    Ap = [Ap; Apsmall]; % Ap = [Ap A^i]
+    Apsmall = Apsmall * A;
+end
+%% Cz
+Cxbar = [];
+Cz2 = [];
+for i = 1:Nc
+    Cxbar = blkdiag(Cxbar, Cx);
+    Cz2 = blkdiag(Cz2, Cu);
+end
+Cz1 = Cxbar*ABbar;
+dxbar = repmat(dx,[Nc 1]);
+dz1 = dxbar - Cxbar*Ap*x0;
+dz2 = repmat(du,[Nc 1]);
+C_hat = [Cz1; Cz2];
+d_hat = [dz1; dz2];
+%% Qbar, Rbar
+Qbar = [];
+Rbar = [];
+for i = 1:Nc
+    Qbar = blkdiag(Qbar, Q);
+    Rbar = blkdiag(Rbar, R);
+end
+%% Final
 uref = zeros(nu,1); %should uref be 0?
 xrefbar = repmat(xref, Nc, 1);
 zrefbar = repmat(uref, Nc, 1);
-q = 0; % ABbar*Qbar*Ap*x0 - ABbar'*Qbar*xrefbar - Rbar*zrefbar
-Qhat = 0; %Rbar + ABbar'*Qbar*ABbar;
+q = ABbar'*Qbar*Ap*x0 - ABbar'*Qbar*xrefbar - Rbar*zrefbar;
+Q_hat = Rbar + ABbar'*Qbar*ABbar;
 %% QP solver
 rel = version('-release');
 rel = rel(1:4); %just the year
 if strcmp(rel,'2011')
-    options = optimset('Algorithm', 'interior-point-convex', 'Display', 'off'); % Matlab 2011
+    options = optimset('Algorithm', 'active-set', 'Display', 'off'); % Matlab 2011
 else
     options = optimoptions('quadprog', ...
-        'Algorithm', 'interior-point-convex', 'Display', 'off'); %Matlab 2013
+        'Algorithm', 'active-set', 'Display', 'off'); %Matlab 2013
 end
 [Z,FVAL,EXITFLAG, OUTPUT] = quadprog(Q_hat, q, C_hat, d_hat, [], [], [], [], [], options);
 %% Return variables
-X = reshape(Z, nu+nx,[]);
+X = reshape(Z, nu,[]);
 u = X(1:nu,:);
-X = X(nu+1:end,:);
 end
 
