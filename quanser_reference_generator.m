f2596c1398b1abc9df1c2a96a7eecb9e675a527f
Ovidiu-Marius Alexandru <ovidiu.marius.alexandru@gmail.com>
2014-08-06 13:42:09 +0200

    Fixed a bug in the reference generator script.

--- quanser_reference_generator.m
+++ quanser_reference_generator.m
@@ -5,17 +5,17 @@ N = 600; % samples
 nx = 6; % states
 nu = 2; % inputs
 uech = [1.8; 1.8]; % The stabilizing command
-savefilename = 'references/ref2.mat';
+savefilename = 'references/ref3.mat';
 %% Run the reference generation Simulink model
 %This creates a timeseries variable, simout, with the reference paths.
 %The first colums is the elevation reference and second is the pitch
 %reference
 sim('reference_generator');
 data = simout.Data(1:N, :)';
-XREF = zeros(nx, N);
-UREF = repmat(uech, [1 N]);
-XREF(1,:) = data(1,:); % import the elevation reference
-XREF(3,:) = data(2,:); % import the pitch reference
+XPATH = zeros(nx, N);
+UPATH = repmat(uech, [1 N]);
+XPATH(1,:) = data(1,:); % import the elevation reference
+XPATH(3,:) = data(2,:); % import the pitch reference
 %% Write file and clean-up
 save(savefilename,'XPATH','UPATH','-v7');
 clear simout data
